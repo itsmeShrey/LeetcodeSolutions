1418164a7f0b2665580cd62db0057ff17f5bf94c
itsmeShrey <91385332+itsmeShrey@users.noreply.github.com>
2023-04-18 22:42:09 +0530

    Time: 3 ms (65.54%), Space: 6 MB (6.76%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,8 +1,23 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        double c=sqrt(x);
-        int d=floor(c);
-        return d;
+        // double c=sqrt(x);
+        // int d=floor(c);
+        // return d;
+
+        int first=1,last=x;
+        while(first<=last){
+            long long int mid=first+(last-first)/2;
+            if(mid*mid==x){
+                return mid;
+            }
+            else if((mid*mid)<x){
+                first=mid+1;
+            }
+            else if((mid*mid)>x){
+                last=mid-1;
+            }
+        }
+        return last;
     }
 };
