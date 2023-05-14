482b50709f47c4bf304baa09c42dbffe12aa0510
itsmeShrey <91385332+itsmeShrey@users.noreply.github.com>
2023-05-14 17:33:31 +0530

    Time: 1106 ms (5.03%), Space: 294.7 MB (72.42%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -11,22 +11,37 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        ListNode* temp=head;
-        vector<int>v;
-        while(temp!=NULL){
-            v.push_back(temp->val);
-            temp=temp->next;
-        }
+        // ListNode* temp=head;
+        // vector<int>v;
+        // while(temp!=NULL){
+        //     v.push_back(temp->val);
+        //     temp=temp->next;
+        // }
 
-        int c=v.size()/2;
-        v.erase(v.begin()+c);
-        ListNode* temp1=NULL;
-        for(int i=v.size()-1;i>=0;i--){
-            ListNode* temp2=new ListNode;
-            temp2->val=v[i];
-            temp2->next=temp1;
-            temp1=temp2;
+        // int c=v.size()/2;
+        // v.erase(v.begin()+c);
+        // ListNode* temp1=NULL;
+        // for(int i=v.size()-1;i>=0;i--){
+        //     ListNode* temp2=new ListNode;
+        //     temp2->val=v[i];
+        //     temp2->next=temp1;
+        //     temp1=temp2;
+        // }
+        // return temp1;
+
+        // Approach 2 (Without using vector)
+
+        if(head->next==NULL){
+            return NULL;
         }
-        return temp1;
+        ListNode* slow=head;ListNode* fast=head;
+
+        while(fast->next->next!=NULL && fast->next->next->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        slow->next=slow->next->next;
+        return head;
+
     }
 };
