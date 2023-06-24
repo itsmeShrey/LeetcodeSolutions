5cab31696bb40515e265e23cda4efb696b603812
itsmeShrey <91385332+itsmeShrey@users.noreply.github.com>
2023-06-24 18:50:48 +0530

    Time: 5 ms (64.45%), Space: 7.6 MB (49.99%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        //alice always wins the game because in any of the turn he will always take the
+        //number with the highest value. So he will always win the game.
+        return true;
+    }
+};
