abb2fc185222b1e9749c25acd6dd3951c44af880
itsmeShrey <91385332+itsmeShrey@users.noreply.github.com>
2023-10-23 09:36:00 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (44.78%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>dp(n+1,0);
+        dp[0]=0;
+        for(int i=0;i<=n;i++){
+            if(i%2==0){
+                dp[i]=dp[i/2];
+            }
+            else if(i%2!=0){
+                dp[i]=dp[i/2]+1;
+            }
+        }
+        return dp;
+    }
+};
