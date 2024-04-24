1af9f79067eeed51da42acbc54fc5ccf8208b4a2
itsmeShrey <91385332+itsmeShrey@users.noreply.github.com>
2024-04-24 14:40:16 +0530

    Time: 3 ms (22.77%), Space: 7 MB (97.26%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector<long long int>v(38,0);
+        v[0]=0,v[1]=1,v[2]=1;
+        for(int i=3;i<v.size();i++){
+            v[i]=v[i-1]+v[i-2]+v[i-3];
+        }
+        return v[n];
+    }
+};
