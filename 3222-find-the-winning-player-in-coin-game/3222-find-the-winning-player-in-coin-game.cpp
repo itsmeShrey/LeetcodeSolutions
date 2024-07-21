b6d3d602411a4cf5ed35c72cb4e618f9e8638475
itsmeShrey <91385332+itsmeShrey@users.noreply.github.com>
2024-07-22 04:44:06 +0530

    Time: 5 ms (100.00%), Space: 7.5 MB (25.00%) - LeetHub

---
+++ 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string losingPlayer(int x, int y) {
+        int turns=min(x,y/4);
+        if((turns)%2==0){
+            return "Bob";
+        }
+        else{
+            return "Alice";
+        }
+    }
+};
